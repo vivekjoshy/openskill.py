df65358f5b8ae604ced31ad017a8bd91d18a53eb
daegontaven <8206808+daegontaven@users.noreply.github.com>
2021-09-03 22:35:06 +0530

    Bump version

--- openskill/__init__.py
+++ openskill/__init__.py
@@ -2,4 +2,4 @@ from openskill.ordinal import ordinal
 from openskill.rate import rate
 from openskill.rating import Rating, create_rating
 
-__version__ = "0.1.0-alpha.1"
+__version__ = "0.1.0-alpha.2"
