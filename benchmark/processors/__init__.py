fe9ebb09c077d1dc2b4881d9f15f89a81ede97cb
Vivek <8206808+vivekjoshy@users.noreply.github.com>
2023-07-10 20:57:05 +0530

    Rewrite Entire API

--- benchmark/processors/__init__.py
+++
@@ -1,3 +0,0 @@
-from .draw import Draw
-from .rank import Rank
-from .win import Win
--- openskill/__init__.py
+++ openskill/__init__.py
@@ -1,12 +1,2 @@
-from openskill.rate import (
-    Rating,
-    create_rating,
-    ordinal,
-    predict_draw,
-    predict_rank,
-    predict_win,
-    rate,
-    team_rating,
-)
 
 __version__ = "4.0.0"
